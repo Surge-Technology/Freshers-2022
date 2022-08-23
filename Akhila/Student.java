a5080e444191a3917ae2fd54faf08048d903355c
Akhilapachava <sts175@surgetechinc.in>
2022-08-23 13:56:15 +0530

    Student

---
+++ Akhila/Student.java
@@ -0,0 +1,40 @@
+public class Student{
+private String Name;
+private int Age;
+private int Mobileno;
+private String Address;
+
+public void setName(String Name)
+{
+this.Name=Name;
+}
+public String getName()
+{
+return Name;
+}
+public void setAge(int Age)
+{
+this.Age=Age;
+}
+public int getAge()
+{
+return Age;
+}
+public void setMobileno(int Mobileno)
+{
+this.Mobileno=Mobileno;
+}
+public int getMobileno()
+{
+return Mobileno;
+} 
+public void setAddress(String Address)
+{
+this.Address=Address;
+}
+public String getAddress()
+{
+return Address;
+} 
+}
+
