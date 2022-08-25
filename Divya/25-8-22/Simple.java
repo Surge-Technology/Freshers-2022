7caf7556beb6fb5c0ae0c53da6bf54d9aae2505a
divya-soundarrajan <STS176@surgetechinc.in>
2022-08-25 17:47:23 +0530

    Constructorr

---
+++ Divya/25-8-22/Simple.java
@@ -0,0 +1,14 @@
+public class Simple{
+	static void add(){
+		System.out.println("divya");
+	}
+	void display1(){
+		System.out.println();
+	}
+public static void main(String[] args){
+		Simple a=new Simple();
+		a.display1();
+		Simple.add();
+		}
+		
+	}
