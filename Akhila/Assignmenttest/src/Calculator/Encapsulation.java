0c2b5e6ea7eab064d86ff2410d0584acd7a240b1
Akhilapachava <sts175@surgetechinc.in>
2022-08-26 12:31:40 +0530

    26/08/22

---
+++ Akhila/Assignmenttest/src/Calculator/Encapsulation.java
@@ -0,0 +1,18 @@
+package Calculator;
+
+public class Encapsulation {
+	private int a;
+	private int b;
+	 public void seta(int a) {
+	        this.a=a;
+	    }
+	 public int geta() {
+	        return a;
+	    }
+	 public void setb(int b) {
+	        this.b=b;
+	    }
+	 public int getb() {
+	        return b;
+	    }
+}
