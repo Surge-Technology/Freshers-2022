67b61e8a75df59009b927bd67e6113009a7061d8
pratikshapatilsurge <sts174@surgetechinc.in>
2022-08-26 12:45:06 +0530

    26/08/2022

---
+++ Pratiksha/Calculator/AbstractCal.java
@@ -0,0 +1,7 @@
+package Calculator;
+
+abstract public class AbstractCal {
+	public abstract void multiplication (int a, int b);
+    public abstract void division (int a, int b);
+    public abstract void mod(int a, int b);
+}
