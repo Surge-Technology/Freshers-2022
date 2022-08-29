b7953223a1341d655ec646ced46f73779c161ff5
Rakeshanand2022 <Rakeshanand_C@surgetechinc.in>
2022-08-29 17:10:43 +0530

    pattern and String Concept

---
+++ Rakesh/29-08-2022/pattern.java
@@ -0,0 +1,16 @@
+package Ifcondition;
+
+public class pattern {
+
+	public static void main(String[] args) 
+	{
+		for (int i = 5; i >= 1; i--) {
+			for(int j=i;j<=5;j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();	
+		}
+		
+	}
+}
