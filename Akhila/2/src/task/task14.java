883102eca51f8006999c06816d7c861c322cab03
Akhilapachava <sts175@surgetechinc.in>
2022-08-31 15:58:29 +0530

    31/08/22

---
+++ Akhila/2/src/task/task14.java
@@ -0,0 +1,16 @@
+// Program to Handle the Unchecked Exceptions
+
+
+package task;
+
+public class task14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        int a[] = { 1, 2, 3, 4 };
+		        System.out.println(a[5]);
+
+	}
+
+}
