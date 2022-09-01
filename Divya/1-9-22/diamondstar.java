f275715f0d4aeff34c1258febca2559dc42258ab
divya-soundarrajan <STS176@surgetechinc.in>
2022-09-01 16:51:50 +0530

    diamondstar

---
+++ Divya/1-9-22/diamondstar.java
@@ -0,0 +1,29 @@
+package Assessment1;
+
+public class diamondstar {
+
+	public static void main(String[] args) {
+		
+		int i,j,k;
+		for(i=1;i<=5;i++) {
+			for(j=4;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(k=1;k<=i;k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		for(i=5;i>=1;i--) {
+			for(j=4;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(k=1;k<=i;k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
