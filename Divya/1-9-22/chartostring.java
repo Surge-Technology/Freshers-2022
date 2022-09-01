27236867ce8d21ddcbb0df4fd895835373d0b4d8
divya-soundarrajan <STS176@surgetechinc.in>
2022-09-01 12:15:46 +0530

    charecter to string

---
+++ Divya/1-9-22/chartostring.java
@@ -0,0 +1,12 @@
+package Assessment1;
+//11) How to Convert Char to String in lava?
+public class chartostring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  char[] ch= {'T','E','M','P','L','E','\t','R','U','N'};
+	        String D=new String(ch);
+	        System.out.println(D);
+	}
+
+}
