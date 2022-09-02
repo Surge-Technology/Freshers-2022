bb91c74edf0a8b00f3ecb62ae29c6c9765932539
Maanraj <111749699+Maanraj@users.noreply.github.com>
2022-09-02 17:35:11 +0530

    Arraylist

---
+++ Maanraj/02-09-2022/ArrayList02.java
@@ -0,0 +1,14 @@
+package arrayList.com;
+
+import java.util.ArrayList;
+
+public class ArrayList02 {
+	public static void main(String[] args) {
+
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Maan");
+		list.add("Madan");
+		list.add("Rakesh");
+		System.out.println(list);
+	}
+}
