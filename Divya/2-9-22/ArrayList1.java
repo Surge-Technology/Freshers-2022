03150eac5c9c02718f9bc588382bda82ed83ea51
divya-soundarrajan <STS176@surgetechinc.in>
2022-09-02 17:44:37 +0530

    arraylist

---
+++ Divya/2-9-22/ArrayList1.java
@@ -0,0 +1,13 @@
+package Assessment1;
+import java.util.*;
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList a=new ArrayList();
+		System.out.println("list of elements:"+a);
+		a.add("divya");
+		a.add("bala");
+	}
+
+}
