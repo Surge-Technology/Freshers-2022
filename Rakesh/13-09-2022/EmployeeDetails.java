e6e1c1d0150b8c3b52c768ed661f37cddd33e0ea
Rakeshanand2022 <Rakeshanand_C@surgetechinc.in>
2022-09-13 16:10:43 +0530

    task 1

---
+++ Rakesh/13-09-2022/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package Sep09;
+
+public class EmployeeDetails {
+	
+	 public int empid;
+	    public String empname;
+	    
+	    public void setemp_id(int empid) {
+	        this.empid=empid;
+	    }
+	    public int getempid() {
+	        return empid;
+	    }
+	    
+	    public void setemp_name(String empname) {
+	        this.empname=empname;
+	    }
+	    public String getempname() {
+	        return empname;
+	    }
+	    
+}
