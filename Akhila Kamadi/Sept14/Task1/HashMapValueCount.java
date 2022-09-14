7553fb70fccfd98ab44d336a0275b7dd39843c25
Akhila-Kamadi-Surge <111122493+Akhila-Kamadi-Surge@users.noreply.github.com>
2022-09-14 11:51:50 +0530

    Create HashMapValueCount.java

---
+++ Akhila Kamadi/Sept14/Task1/HashMapValueCount.java
@@ -0,0 +1,35 @@
+package com.Surge.Sept14.Task1;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class HashMapValueCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> map = new HashMap<>();
+		map.put(1, "TCS");
+		map.put(2, "Wipro");
+		map.put(3, "Google");
+		map.put(4, "TCS");
+		map.put(5, "Google");
+		map.put(6, "Google");
+		map.put(7, "Google");
+		
+		HashMap<String, Integer> map2 = new HashMap<String, Integer>();
+		for (Entry<Integer, String> entry : map.entrySet()) {
+//			Integer key = entry.getKey();
+			String val = entry.getValue();
+			
+			if(map2.containsKey(val)) {
+				int Key = map2.get(val);
+				map2.put(val, Key+1);
+			}
+			else {
+				map2.put(val, 1);
+			}
+		}
+		
+		System.out.println(map2);
+	}
+}
