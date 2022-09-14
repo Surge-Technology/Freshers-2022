ca9c24454bf2af693c660ef439ec4581cc21b23f
Balamanchari1248 <111047318+Balamanchari1248@users.noreply.github.com>
2022-09-14 10:15:37 +0530

    mapping and string

---
+++ Balamanchari/08-09-22/EmpnameList.java
@@ -0,0 +1,29 @@
+package eightnine;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class EmpnameList {
+	
+	int emp_id;
+	String emp_name;
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	public String getEmp_name() {
+		return emp_name;
+	}
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+	public void EmpnameList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
