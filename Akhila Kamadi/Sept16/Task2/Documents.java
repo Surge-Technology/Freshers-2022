2d746c55d6a32438e88efbe9a4a39c53d21c7cf2
Akhila-Kamadi-Surge <111122493+Akhila-Kamadi-Surge@users.noreply.github.com>
2022-09-17 11:03:41 +0530

    Modified employee getter setter

---
+++ Akhila Kamadi/Sept16/Task2/Documents.java
@@ -0,0 +1,21 @@
+package com.Surge.Sept16.Task2;
+
+class Documents{
+	private String documentName, documentID;
+
+	public String getDocumentName() {
+		return documentName;
+	}
+
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+
+	public String getDocumentID() {
+		return documentID;
+	}
+
+	public void setDocumentID(String documentID) {
+		this.documentID = documentID;
+	}
+}
