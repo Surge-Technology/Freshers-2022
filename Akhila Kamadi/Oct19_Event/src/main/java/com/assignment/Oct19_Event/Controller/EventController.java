a6943d746a745e6248295a83832f8be12a305c6b
Akhila-Kamadi-Surge <111122493+Akhila-Kamadi-Surge@users.noreply.github.com>
2022-10-20 11:08:40 +0530

    updated event

--- Akhila Kamadi/Oct19_Event/src/main/java/com/assignment/Oct19_Event/Controller/EventController.java
+++ Akhila Kamadi/Oct19_Event/src/main/java/com/assignment/Oct19_Event/Controller/EventController.java
@@ -3,7 +3,12 @@ package com.assignment.Oct19_Event.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.assignment.Oct19_Event.Entity.Event;
@@ -17,6 +22,21 @@ public class EventController {
 	
 	@GetMapping("/event")
 	public List<Event> getEvent(){
-		return eventService.findAll();
+		return eventService.getAllEvents();
+	}
+	
+	@PostMapping("/event/insert")
+	public void insertEvent(@RequestBody Event event) {
+		eventService.saveEvent(event);
+	}
+	
+	@PutMapping("/event/update")
+	public void updateEvent(@RequestBody Event event) {
+		eventService.saveEvent(event);
+	}
+	
+	@DeleteMapping("/event/delete/{id}")
+	public void deleteEvent(@PathVariable Long id) {
+		eventService.deleteEvent(id);
 	}
 }
--- Akhila Kamadi/Oct19_Event/src/main/java/com/assignment/Oct19_Event/Service/EventService.java
+++ Akhila Kamadi/Oct19_Event/src/main/java/com/assignment/Oct19_Event/Service/EventService.java
@@ -2,6 +2,7 @@ package com.assignment.Oct19_Event.Service;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +15,20 @@ public class EventService {
 	@Autowired
 	private EventRepository eventRepository;
 	
-	public List<Event> findAll() {
+	public List<Event> getAllEvents() {
 		return eventRepository.findAll();
 	}
+	
+	public void saveEvent(Event event) {
+		eventRepository.save(event);//here id is null
+	}
+	
+	public void updateEvent(Event event) {
+		eventRepository.save(event);//here id is present -> not null
+	}
 
+	public void deleteEvent(long id) {
+		eventRepository.deleteById(id);
+	}
+	
 }
