13bc53e5eca9bb6ca478ca1b038fe4625b95946e
Akhila-Kamadi-Surge <111122493+Akhila-Kamadi-Surge@users.noreply.github.com>
2022-10-21 12:18:16 +0530

    updated

--- Akhila Kamadi/Oct19_Event/src/main/java/com/assignment/Oct19_Event/Controller/EventController.java
+++ Akhila Kamadi/Oct19_Event/src/main/java/com/assignment/Oct19_Event/Controller/EventController.java
@@ -3,6 +3,8 @@ package com.assignment.Oct19_Event.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,5 +40,20 @@ public class EventController {
 	@DeleteMapping("/event/delete/{id}")
 	public void deleteEvent(@PathVariable Long id) {
 		eventService.deleteEvent(id);
+	}
+	
+	@GetMapping("/event/getByName/{eventName}")
+	public ResponseEntity<List<Event>> getByEventName(@PathVariable String eventName){
+		return new ResponseEntity<List<Event>>(eventService.getEventByName(eventName),HttpStatus.OK);
+	}
+	
+	@GetMapping("/event/getByLocation/{eventLocation}")
+	public ResponseEntity<List<Event>> getByEventLocation(@PathVariable String eventLocation){
+		return new ResponseEntity<List<Event>>(eventService.getEventByLocation(eventLocation),HttpStatus.OK);
+	}
+	
+	@GetMapping("/event/getByOrganizer/{eventOrganizer}")
+	public ResponseEntity<List<Event>> getEventByOrganizer(@PathVariable String eventOrganizer){
+		return new ResponseEntity<List<Event>>(eventService.getEventByOrganizer(eventOrganizer),HttpStatus.OK);
 	}
 }
--- Akhila Kamadi/Oct19_Event/src/main/java/com/assignment/Oct19_Event/Service/EventService.java
+++ Akhila Kamadi/Oct19_Event/src/main/java/com/assignment/Oct19_Event/Service/EventService.java
@@ -2,33 +2,21 @@ package com.assignment.Oct19_Event.Service;
 
 import java.util.List;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.assignment.Oct19_Event.Entity.Event;
-import com.assignment.Oct19_Event.Repository.EventRepository;
 
-@Service
-public class EventService {
-	
-	@Autowired
-	private EventRepository eventRepository;
-	
-	public List<Event> getAllEvents() {
-		return eventRepository.findAll();
-	}
-	
-	public void saveEvent(Event event) {
-		eventRepository.save(event);//here id is null
-	}
-	
-	public void updateEvent(Event event) {
-		eventRepository.save(event);//here id is present -> not null
-	}
+public interface EventService {
 
-	public void deleteEvent(long id) {
-		eventRepository.deleteById(id);
-	}
+	List<Event> getAllEvents();
 	
+	void saveEvent(Event event);
+	
+	void updateEvent(Event event);
+	
+	void deleteEvent(long id);
+	
+	List<Event> getEventByName(String eventName);
+	
+	List<Event> getEventByLocation(String eventLocation);
+	
+	List<Event> getEventByOrganizer(String eventOrganizer);
 }
